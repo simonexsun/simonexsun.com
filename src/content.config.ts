5cd86879591b621d3a8799f3bf8c310ebe10d1e8
Simone Sun <simonexsun@gmail.com>
2025-11-30 01:15:25 -0500

    Relax schema validation

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,26 +12,7 @@ const projects = defineCollection({
         cover: image(),
         coverAlt: z.string(),
         date: z.date(),
-        client: z.string().optional(),
-        overview: z.string().optional(), // A longer summary of the project
-        role: z.string(), // short description of the type of work I did, e.g. "UI/UX Design", "Development", etc.
-        type: z.string().optional(), // e.g. "Full time", "Contract", "Internship"
-        timeline: z.string().optional(), // e.g. "3 months", "6 weeks"
-        team: z.string(), // e.g. "co-founder, UI designer, engineer"
-        tools: z.string(), // e.g. "Figma, Astro, Three.js"
-        display: z.enum(['Side Project', 'Selected Work']).default('Selected Work'),
-
-
-        /*
-        name: z.string(),
-        type: z.enum(['Space Probe', 'Mars Rover', 'Comet Lander']),
-        launch_date: z.date(),
-        status: z.enum(['Active', 'Inactive', 'Decommissioned']),
-        destination: z.string(),
-        operator: z.string(),
-        notable_discoveries: z.array(z.string()),
-        */
-      }),
+      }).passthrough(), // Allow additional fields in markdown without validation
   });
 
   export const collections = { projects };
